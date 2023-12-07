815e39f52a07ac78859d0da51a77c0a36e9a04fc
trap20 <psd357@posteo.de>
2023-12-07 13:58:26 +0100

    Minor fixes
    -fix generation length
    -load tokenizer config from model

--- modules/mamba.py
+++ modules/mamba.py
@@ -11,16 +11,19 @@ from modules.logging_colors import logger
 from modules.text_generation import get_max_prompt_length
 from modules.callbacks import Iteratorize
 
+
 class MambaSsmModel:
+
     def __init__(self):
         pass
 
     @classmethod
     def from_pretrained(self, path_to_model):
-        
+
         dtype = torch.float16
         model = MambaLMHeadModel.from_pretrained(path_to_model, "cuda", dtype)
-        tokenizer = AutoTokenizer.from_pretrained("EleutherAI/gpt-neox-20b")
+        tokenizer = AutoTokenizer.from_pretrained(path_to_model)
+        # logger.debug("mamba: tokenizer loaded %s", tokenizer)
 
         result = self()
         result.model = model
@@ -31,22 +34,22 @@ class MambaSsmModel:
         return result, tokenizer
 
     def encode(self, string, **kwargs):
-        return self.tokenizer.encode(string, return_tensors= 'pt')
+        return self.tokenizer.encode(string, return_tensors='pt')
 
     def decode(self, ids, **kwargs):
         return self.tokenizer.decode(ids, decode_special_tokens=True)
 
     def get_logits(self, token_ids, **kwargs):
-        logger.debug("mamba: token_ids %s",token_ids)
+        logger.debug("mamba: token_ids %s", token_ids)
         output = self.model.generate(
             input_ids=token_ids.cuda(),
-            max_length = 8,
+            max_length=8,
             cg=True,
             return_dict_in_generate=True,
             output_scores=True,
             enable_timing=False,
         )
-        logger.debug("mamba: output %s",output)
+        logger.debug("mamba: output %s", output)
         logger.debug("mamba: output.scores %s", output.scores)
         scores = output.scores[0]
         logger.debug("scores %s", scores)
@@ -58,10 +61,11 @@ class MambaSsmModel:
 
     def generate(self, prompt, state, callback=None):
         input_ids = self.encode(prompt)
+        initial_len = len(input_ids[0])
 
         output = self.model.generate(
             input_ids=input_ids.cuda(),
-            max_length=state['max_new_tokens'],
+            max_length=state['max_new_tokens'] + initial_len,
             cg=True,
             return_dict_in_generate=True,
             output_scores=True,
@@ -70,8 +74,8 @@ class MambaSsmModel:
             top_k=state['top_k'],
             top_p=state['top_p'],
         )
-        initial_len = len(input_ids[0])
         decoded = self.decode(output.sequences.cpu()[0][initial_len:])
+        # logger.debug("decoded %s", decoded)
         callback(decoded)
         return decoded
 
@@ -82,11 +86,10 @@ class MambaSsmModel:
 
         #     output += token
 
-
     def generate_with_streaming(self, *args, **kwargs):
-        with Iteratorize(self.generate, args, kwargs, callback=None) as generator:
+        with Iteratorize(self.generate, args, kwargs,
+                         callback=None) as generator:
             reply = ''
             for token in generator:
                 reply += token
                 yield reply
-
